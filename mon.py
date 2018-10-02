8a10584a5922ae29264229ea4e102e82a0482962
Alexander Grynchuk <agrynchuk@gmail.com>
2018-10-02 16:09:47 +0300

    Add logging

--- mon.py
+++ mon.py
@@ -5,9 +5,15 @@ import os
 from Exscript import Host, Account
 from Exscript.protocols import SSH2, Telnet
 from Exscript.protocols.drivers.nxos import NXOSDriver
-from Exscript.protocols.drivers import add_driver, disable_driver
+from Exscript.protocols.drivers import add_driver
+from Exscript.protocols.exception import ProtocolException, TimeoutException
+
 import yaml
 import time
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class MyNXOSDriver(NXOSDriver):
@@ -19,6 +25,7 @@ class MyNXOSDriver(NXOSDriver):
 add_driver(MyNXOSDriver)
 MAIN_SECTION = 'main'
 
+
 def pars_host_str(host_str):
     host_str_splited = host_str.split(':')
     host = host_str_splited[0]
@@ -26,7 +33,7 @@ def pars_host_str(host_str):
     return host, port
 
 
-class ConnectionError(Exception):
+class DevConnectionError(Exception):
     pass
 
 
@@ -50,24 +57,27 @@ class Device(object):
         return uri
 
     def connect(self):
+        logger.warning('start connecting to {}'.format(self.get_uri()))
         self._exscript_host = Host(uri=self.get_uri())
         self._exscript_account = Account(name=self.login, password=self.password)
         self._exscript_host.set_account(self._exscript_account)
 
         if self.protocol == self._exscript_host.get_protocol():
-            self._conn = Telnet(debug=5)
+            self._conn = Telnet(debug=0)
         elif self.protocol == self._exscript_host.get_protocol():
             self._conn = SSH2()
         self._conn.set_driver(driver=self.type)
         try:
             self._conn.connect(hostname=self._exscript_host.get_address(), port=int(self._exscript_host.get_tcp_port()))
+            logger.warning('connection established with {}'.format(self.get_uri()))
         except OSError as e:
-            raise ConnectionError(str(e))
+            raise DevConnectionError('{} {}'.format(self.get_uri(), str(e)))
 
         time.sleep(5)
         self._conn.send('\r')
-
+        logger.warning('start login to {}'.format(self.get_uri()))
         self._conn.login(account=self._exscript_account)
+        logger.warning('login to {} success'.format(self.get_uri()))
         self._conn.autoinit()
 
     def show_version(self):
@@ -83,8 +93,10 @@ class Device(object):
         return conf
 
     def dump_show_run(self, path):
+        logger.warning('Start dumping show run {} to "{}" file'.format(self.get_uri(), path))
         with open(path, 'w') as cnf_:
             cnf_.write(self.show_run())
+        logger.warning('show run {} dumped to "{}" file success'.format(self.get_uri(), path))
 
 
 class DeviceScope(object):
@@ -93,10 +105,12 @@ class DeviceScope(object):
         self.data_dir = '.'
 
     def load_yaml(self, path):
+        logger.warning('loading config "{}"'.format(path))
         with open(path) as _:
             data = yaml.load(_)
-
+        logger.warning('config "{}" loaded success'.format(path))
         if MAIN_SECTION in data:
+            logger.warning('detected main in config'.format(path))
             main = data.pop(MAIN_SECTION)
             self.data_dir = main.get('data', self.data_dir)
 
@@ -114,8 +128,17 @@ class DeviceScope(object):
 
     def dump(self):
         for device in self.scope:
-            device.connect()
-            device.dump_show_run(path=os.path.abspath(os.path.join(self.data_dir, device.name + '.cnf')))
+            try:
+                device.connect()
+                device.dump_show_run(path=os.path.abspath(os.path.join(self.data_dir, device.name + '.cnf')))
+            except DevConnectionError as e:
+                logger.error(e)
+            except (ProtocolException, TimeoutException) as e:
+                logger.error('{} {} {}'.format(device.get_uri(), device.proxy, e))
+
+            # TODO: to be fixed in next version exscript
+            except UnboundLocalError as e:
+                logger.error('Connection refused {}'.format(device.get_uri()))
 
 
 if __name__ == '__main__':
